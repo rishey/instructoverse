3f7709e7170f6621a799dc91b00899ea713ef830
richard bluestein <richard@bluestein.com>
2013-10-11 10:52:12 -0500

    test for ominiauth ignore in gitignore

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET']#, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
  
+
+ #test
