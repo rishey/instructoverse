831e71001c0fb91c359f5e24f7b1d24a3994f00c
richard bluestein <richard@bluestein.com>
2013-10-11 11:28:20 -0500

    added initializer back to gitignore after removing keys

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-
     redirect_to :root 
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,37 @@
 class UsersController < ApplicationController
 
-  def index
-    redirect_to :root
-  end
+  # def index
+  #   redirect_to :root
+  # end
 
-  def show
-    if current_user
-      if params[:id].to_i != current_user.id
-        @error = "You can view your profile only."
-        render 'homes/index'
-      end
-    else
-      @error = "You need to be logged in to view your profile"
-      render 'sessions/new'
-    end
-  end
+  # def show
+  #   if current_user
+  #     if params[:id].to_i != current_user.id
+  #       @error = "You can view your profile only."
+  #       render 'homes/index'
+  #     end
+  #   else
+  #     @error = "You need to be logged in to view your profile"
+  #     render 'sessions/new'
+  #   end
+  # end
 
-  # sign up 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user 
-    else
-      @signup_errors = @user.errors.full_messages
-      render "sessions/new"
-    end 
-  end 
+  # # sign up 
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect_to @user 
+  #   else
+  #     @signup_errors = @user.errors.full_messages
+  #     render "sessions/new"
+  #   end 
+  # end 
 
-  private 
+  # private 
 
-    def user_params
-      params.require(:signup).permit(:username, :email, :password, :password_confirmation)
-    end
+  #   def user_params
+  #     params.require(:signup).permit(:username, :email, :password, :password_confirmation)
+  #   end
 
 end 
