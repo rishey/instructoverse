14d8aeab864e6d8101bc38df1805b5bcb9fd9c62
richard bluestein <richard@bluestein.com>
2013-10-11 10:43:40 -0500

    setting up oauth for fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,20 @@ class User < ActiveRecord::Base
 	has_many :created_courses, class_name: "Course", foreign_key: :creator_id
 	has_many :enrollments
 	has_many :attended_courses, through: :enrollments, source: :course
-  has_secure_password
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username, :email
+  # has_secure_password
+  # validates_presence_of :username, :email
+  # validates_uniqueness_of :username, :email
+
+   def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      # user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+  
 end
